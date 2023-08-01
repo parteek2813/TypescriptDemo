8ad2f5ee8e65798f7e9720ac11c42f1892fa84cb
parteek kumar <digitalparteek@gmail.com>
2023-08-01 16:53:01 +0530

    Enums in ts

---
+++ myEnums.js
@@ -0,0 +1,13 @@
+// const AISLE = 0;
+// const MIDDLE = 1;
+// const WINDOW = 2;
+// if(seat === 0){
+// }
+var Seatchoice;
+(function (Seatchoice) {
+    Seatchoice["AISLE"] = "10";
+    Seatchoice["MIDDLE"] = "MIDDLE";
+    Seatchoice["WINDOW"] = "dsad";
+})(Seatchoice || (Seatchoice = {}));
+var hcSeat = Seatchoice.AISLE;
+var hSeat = Seatchoice.MIDDLE;
---
+++ myEnums.ts
@@ -0,0 +1,18 @@
+// const AISLE = 0;
+// const MIDDLE = 1;
+// const WINDOW = 2;
+
+// if(seat === 0){
+
+// }
+
+
+enum Seatchoice {
+    AISLE="10", MIDDLE="MIDDLE", WINDOW="dsad"
+}
+
+
+const hcSeat = Seatchoice.AISLE;
+const hSeat = Seatchoice.MIDDLE; 
+
+export {}
