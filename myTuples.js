8d4e8c6472532e326dc3acc94b652f8a6398770e
parteek kumar <digitalparteek@gmail.com>
2023-08-01 16:38:51 +0530

    Tuples in ts

---
+++ myTuples.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// const User: (string | number)[] = [1,"ab"];
+var User1;
+User1 = ["eas", 23123, true];
+var rgb = [321, 321, 321, 32];
+var newUser = [12, "a@b.com"];
+newUser[1] = "a@c.com";
---
+++ myTuples.ts
@@ -0,0 +1,18 @@
+// const User: (string | number)[] = [1,"ab"];
+let User1: [string, number, boolean] 
+
+User1 = ["eas", 23123, true]
+
+
+let rgb: [number, number, number , number] = [321,321,321,32]
+
+type User = [number , string];
+
+const newUser: User = [12,"a@b.com"]
+
+
+newUser[1] = "a@c.com";
+
+// newUser.push(true)
+
+export {}
