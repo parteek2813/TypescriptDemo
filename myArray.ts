7de8859384a5b5c5c883de3b7a2ccdfe61179d73
parteek kumar <digitalparteek@gmail.com>
2023-08-01 16:07:07 +0530

    Array definition

---
+++ myArray.ts
@@ -0,0 +1,24 @@
+const superHeros: string[] = [];
+// const heroPower: number[] = [];
+// Or
+const heroPower: Array<number> = [];
+
+
+type User1 = {
+    name: string,
+    isActive: boolean,
+
+}
+
+const AllUsers: User1[] = [];
+
+const MlModels: number[][] = [
+    [255,255,255],[255,224,255],
+];
+
+superHeros.push("spiderman");
+heroPower.push(1);
+
+
+AllUsers.push({name: "spiderman", isActive:true})
+
